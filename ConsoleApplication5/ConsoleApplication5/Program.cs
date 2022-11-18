d6bc9ce7e196e706db474bcd848adb3d2bc6c958
kulaeva200 <118422378+kulaeva200@users.noreply.github.com>
2022-11-18 15:14:11 +0900

    Add files via upload

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            float a, b, sum;
+
+            Console.WriteLine("Введите первое число для сложения:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите первое число для сложения:");
+            b = Convert.ToInt32(Console.ReadLine());
+            sum = a + b;
+            Console.WriteLine("Сумма данных чисел:");
+            Console.WriteLine(sum);
+            float y, z, x;
+            Console.WriteLine("Введите первое число для произведения:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите первое число для произведения:");
+            z = Convert.ToInt32(Console.ReadLine());
+            x = y * z;
+            Console.WriteLine("Произведение данных чисел:");
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+
+        }
+    }
+}
